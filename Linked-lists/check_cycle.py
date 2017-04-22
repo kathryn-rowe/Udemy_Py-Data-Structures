e806ea241bc882a327e2a7e7b5ba87143fb83efe
Computer123 <kjrowe06@gmail.com>
2017-04-22 10:18:16 -0700

    Practice linked lists

--- Linked-lists/check_cycle.py
+++ Linked-lists/check_cycle.py
@@ -1,25 +1,54 @@
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = next
+
+
 def check_cycle(node):
+    """Run through single-linked list, confirm if it cycles (return False if not), return data where cycle starts
+
+    >>> a = Node(1)
+    >>> b = Node(2)
+    >>> c = Node(3)
+
+    >>> a.nextnode = b
+    >>> b.nextnode = c
+    >>> c.nextnode = a
+
+    >>> check_cycle(a)
+    1
+    """
+
     # Begin both markers at the first node
     marker1 = node
     marker2 = node
 
     # Go until end of list
-    while marker2 != None and marker2.nextnode != None:
-        
+    while marker2 is not None and marker2.nextnode is not None:
+
         marker1 = marker1.nextnode
         marker2 = marker2.nextnode.nextnode
 
         # Check if the markers have matched
         if marker2 == marker1:
-            return True
+            # Return True if the question asks if it is a cycle
+            # return True
+
+            marker1 = node
+
+            while marker1 != marker2:
+                marker1 = marker1.next
+                marker2 = marker2.next
+            return marker1.data
 
     # Case where marker ahead reaches the end of the list
     return False
 
-a = Node(1)
-b = Node(2)
-c = Node(3)
 
-a.nextnode = b
-b.nextnode = c
-c.nextnode = a
+if __name__ == "__main__":
+    print
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "*** ALL TESTS PASSED ***"
+    print
